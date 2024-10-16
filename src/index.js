06d908febb9f6ed2e7cabba32f9647f1b4c16875
Rolyamparo <143687528+Rolyamparo@users.noreply.github.com>
2024-10-17 03:06:53 +0800

    index.js update

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // Ensure ReactDOM is imported
+import './index.css'; // If you have a global stylesheet
+// import App from './App'; // Assuming you have an App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
+
